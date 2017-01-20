0661867ab0f87e1f7220b0c75b02a4c5f807db70
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-01-20 16:16:10 +0100

    Plot flux instead of velocity.
    To match plots from the SHMIP website.

--- plot.py
+++ plot.py
@@ -7,12 +7,15 @@ nc = nc4.Dataset('run-extra.nc')
 x = nc.variables['x'][:]*1e-3
 p = nc.variables['effbwp'][-1,:,:]
 v = nc.variables['bwatvel[0]'][-1,:,:]
+w = nc.variables['bwat'][-1,:,:]
 
 # calc compute min, max and mean
 pmin = p.min(axis=1)
 pmax = p.max(axis=1)
 pavg = p.mean(axis=1)
 vavg = v.mean(axis=1)
+wavg = w.mean(axis=1)
+qavg = -wavg*vavg/(365.0*24*60*60)
 
 # assert there are no longitudinal variations
 assert (pmax-pmin).max() == 0.0
@@ -23,15 +26,15 @@ fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 # plot effective pressure
 ax1.plot(x, pavg*1e-6)
 ax1.set_ylim(0, 4.0)
-ax1.set_ylabel('Water effective pressure (MPa)')
+ax1.set_ylabel('Effective pressure (MPa)')
 ax1.grid()
 
 # plot water velocity
-ax2.plot(x, -vavg*1e-6)
+ax2.plot(x, qavg*1e3)
 ax2.set_xlim(0.0, 100.0)
 ax2.set_ylim(0.0, 1.0)
 ax2.set_xlabel('Distance from ice margin (km)')
-ax2.set_ylabel('Water velocity ($10^6\,m\,a^{-1}$)')
+ax2.set_ylabel('Water flux ($10^{-3}\,m^2\,s^{-1}$)')
 ax2.grid()
 
 # save
