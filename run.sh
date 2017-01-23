f97a53dbd2f3e0ca69ad933db3d5f4fd629ee0ec
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-01-23 22:12:18 +0100

    Adapt run script for exp. E1 to E5.

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# executables
+# Environment variables
+# ---------------------
+
 : ${PISM_DO:=""}                # use "echo" to just display the command
 : ${PISM_MPIDO:=""}             # MPI command for parallel run
 : ${PISM_EXEC:="pismr"}         # name of PISM executable
+
+
+# Command-line argument parser
+# ----------------------------
 
 # loop on keyword, argument pairs
 while [[ $# -gt 0 ]]
@@ -29,6 +35,26 @@ done
 exp=${exp:="a1"}
 years=${years:="5"}
 
+# Experiment specific settings
+# ----------------------------
+
+if [ "${exp:0:1}" == "e" ]
+then
+    boot_file="input/boot_$exp.nc"
+    melt_file="input/melt_e1.nc"
+    mx=303
+    my=55
+else
+    boot_file="input/boot_sqrt.nc"
+    melt_file="input/melt_$exp.nc"
+    mx=403
+    my=41
+fi
+
+
+# Output settings
+# ---------------
+
 # run name
 mkdir -p output
 run="output/$exp"
@@ -39,14 +65,18 @@ extra_vars=bwat,bwatvel,bwp,bwprel,effbwp,wallmelt  # diagnostics
 ts_vars=hydro_ice_free_land_loss,hydro_ice_free_land_loss_cumulative,
 ts_vars+=hydro_negative_thickness_gain,hydro_negative_thickness_gain_cumulative
 
+
+# Actual run
+# ----------
+
 # build config file
 ncgen config.cdl -o config.nc
 
 # run PISM
 $PISM_DO $PISM_MPIDO $PISM_EXEC \
     -config_override config.nc -report_mass_accounting \
-    -i input/boot_sqrt.nc -bootstrap -hydrology_bmelt_file input/melt_$exp.nc \
-    -Mx 403 -My 41 -Mz 2 -Lz 2000 -y $years -o $run.nc -o_size small \
+    -i $boot_file -bootstrap -hydrology_bmelt_file $melt_file \
+    -Mx $mx -My $my -Mz 2 -Lz 2000 -y $years -o $run.nc -o_size small \
     -extra_file ${run}_extra.nc -extra_times monthly -extra_vars $extra_vars \
     -ts_file ${run}_ts.nc -ts_times daily -ts_vars $ts_vars \
     > $run.log 2> $run.err
