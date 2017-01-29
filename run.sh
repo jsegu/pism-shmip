8c2a069ac21a42e37187069469fec58f0aa65738
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-01-29 11:39:53 +0100

    Add alternative config with tillwat_max = 2.0 m.

--- run.sh
+++ run.sh
@@ -19,6 +19,10 @@ do
             exp="$2"
             shift
             ;;
+        -c|--config)
+            config="$2"
+            shift
+            ;;
         -y|--years)
             years="$2"
             shift
@@ -34,6 +38,7 @@ done
 # default arguments
 exp=${exp:="a1"}
 years=${years:="5"}
+config=${config:=""}
 
 # Experiment specific settings
 # ----------------------------
@@ -55,12 +60,13 @@ fi
 # Output settings
 # ---------------
 
-# run name
+# run name and config file
 mkdir -p output
-run="output/$exp"
+run="output/$exp${config:+_$config}"
+conf_file="config${config:+_$config}"
 
 # extra and ts variables
-extra_vars=bwat,bwatvel,bwp,bwprel,effbwp,wallmelt  # diagnostics
+extra_vars=bwat,bwatvel,bwp,bwprel,effbwp,tauc,wallmelt  # diagnostics
 #extra_vars+=,hydrobmelt,hydroinput,hydrovelbase_mag  # verification
 ts_vars=hydro_ice_free_land_loss,hydro_ice_free_land_loss_cumulative,
 ts_vars+=hydro_negative_thickness_gain,hydro_negative_thickness_gain_cumulative
@@ -70,11 +76,11 @@ ts_vars+=hydro_negative_thickness_gain,hydro_negative_thickness_gain_cumulative
 # ----------
 
 # build config file
-ncgen config.cdl -o config.nc
+ncgen $conf_file.cdl -o $conf_file.nc
 
 # run PISM
 $PISM_DO $PISM_MPIDO $PISM_EXEC \
-    -config_override config.nc -report_mass_accounting \
+    -config_override $conf_file.nc -report_mass_accounting \
     -i $boot_file -bootstrap -hydrology_bmelt_file $melt_file \
     -Mx $mx -My $my -Mz 2 -Lz 2000 -y $years -o $run.nc -o_size small \
     -extra_file ${run}_extra.nc -extra_times monthly -extra_vars $extra_vars \
